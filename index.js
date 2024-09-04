e4d6e493b8964e204d1bf23375987ff732e8d9da
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-04 15:38:36 -0600

    Add the pseudocode to index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+/* Pseudocode
+Declare 4 variables, one string, and three equations using a unique arithmetic operator.
+The string will be a message to alert the user of their task.
+Finally use an html tag to display the final vault code
+*/
